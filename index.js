0134e730aa84166110f5b0377d94fdb108b80c85
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2022-04-14 02:28:36 +0500

    commit

--- index.js
+++ index.js
@@ -14,14 +14,19 @@ const errorHandler = require('./src/middleware/ErrorHandlingMiddleware');
 const app = express();
 // app.use(cors());
 app.use((req, res, next) => {
-  console.log(req);
   res.header('Access-Control-Allow-Origin', '*');
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  // );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+  );
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Cache-Control'
   );
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-
   next();
 });
 const corsOptions = {
