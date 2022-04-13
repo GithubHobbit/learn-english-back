f524f64a25d033cd8baaed226127f711ca67b793
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2022-04-14 00:31:14 +0500

    commit

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,multipart/form-data');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
