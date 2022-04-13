8a6460aa0c1db550f2b538821fcc036a2b8d8e61
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2022-04-14 02:58:59 +0500

    commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   // );
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, Accept, X-Requested-With, Content-Type, Authorization, Cache-Control, Access-Control-Allow-Origin'
+    'Origin, Accept, X-Requested-With, Content-Type, Authorization, Cache-Control'
   );
   res.header(
     'Access-Control-Allow-Methods',
